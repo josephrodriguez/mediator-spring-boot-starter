00e336b914e888d1a421f0eb681ee53b026685d0
Jose Miguel Rodriguez <josemrodriguez89@gmail.com>
2022-03-09 14:02:26 -0500

    import configuration class with annotation

--- src/main/java/com/aureum/springboot/annotations/EnableMediator.java
+++ src/main/java/com/aureum/springboot/annotations/EnableMediator.java
@@ -1,9 +1,13 @@
 package com.aureum.springboot.annotations;
 
+import com.aureum.springboot.config.SpringBootMediatorAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
 import java.lang.annotation.*;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Import(SpringBootMediatorAutoConfiguration.class)
 public @interface EnableMediator {
 }
