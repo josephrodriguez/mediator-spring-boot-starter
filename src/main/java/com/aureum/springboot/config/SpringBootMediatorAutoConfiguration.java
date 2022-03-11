eaf133945339efacb552904234cfc52ecc0448cd
Jose Miguel Rodriguez <josemrodriguez89@gmail.com>
2022-03-11 18:16:51 -0500

    configure Mediator bean

--- src/main/java/com/aureum/springboot/config/SpringBootMediatorAutoConfiguration.java
+++ src/main/java/com/aureum/springboot/config/SpringBootMediatorAutoConfiguration.java
@@ -1,8 +1,24 @@
 package com.aureum.springboot.config;
 
+import com.aureum.springboot.service.Mediator;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ *
+ */
 @Configuration
 public class SpringBootMediatorAutoConfiguration {
+
+    /**
+     * @param factory
+     * @return
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public Mediator mediator(ListableBeanFactory factory) {
+        return new Mediator(factory);
+    }
 }
